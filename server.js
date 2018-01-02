812a009ceb46c438d5f73833dd18138bcd1674ce
Jay "Danger" Farnsworth <DrDanger@Jays-MacBook-Pro.local>
2018-01-02 16:27:59 -0700

    fixed server

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
+const http        = require('http')
 const express = require("express");
-const cors = require("cors");
+const bodyParser  = require('body-parser')
+const morgan      = require('morgan')
+const cors        = require('cors')
+const app         = module.exports = express()
+const server      = http.createServer(app)
+const port        = parseInt(process.env.PORT || 3000)
+const devMode     = process.env.NODE_ENV !== 'production'
 
-const app = express();
-app.use(cors());
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan(devMode ? 'dev' : 'combined'))
+app.use(cors({origin: true}))
 
 var data = [{
   actors: ['Testy McTest', 'Rob Schnider'],
   directors: ['Stanley Kubrick', 'Dr. Testenstein']
 }];
-
+app.post("/", function(request, response) {
+  response.json({data});
+})
 app.get("/", function(request, response) {
   response.json({data});
 })
 
-app.listen(process.env.PORT || 3000);
+app.use(notFound)
+app.use(errorHandler)
+
+server.listen(port)
+  .on('error',     console.error.bind(console))
+  .on('listening', console.log.bind(console, 'Listening on ' + port));
+
+function notFound(req, res, next) {
+  const url = req.originalUrl
+  if (!/favicon\.ico$/.test(url) && !/robots\.txt$/.test(url)) {
+    // Don't log less important auto requests
+    console.error('[404: Requested file not found] ', url)
+  }
+  res.status(404).send({error: 'Url not found', status: 404, url})
+}
+
+function errorHandler(err, req, res, next) {
+  console.error('ERROR', err)
+  const stack =  devMode ? err.stack : undefined
+  res.status(500).send({error: err.message, stack, url: req.originalUrl})
+}

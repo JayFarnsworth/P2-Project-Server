1713166aad58b1e03b506339dbd4a0faf1096f57
Jay "Danger" Farnsworth <DrDanger@Jays-MacBook-Pro.local>
2018-01-12 11:18:01 -0700

    committing shit

--- server.js
+++ server.js
@@ -15,14 +15,82 @@ app.use(morgan(devMode ? 'dev' : 'combined'))
 app.use(cors({origin: true}))
 
 var data = [{
-  actors: ['Testy McTest', 'Rob Schnider'],
-  directors: ['Stanley Kubrick', 'Dr. Testenstein']
+  currentUser: 'N/A',
+  profiles: ['Rex'],
+  users: {
+    Rex: {
+      actors: ['Rob Schnider', 'George Clooney'],
+      directors: ['Stanley Kubrick', 'Wes Anderson'],
+      movies: ['The Departed']
+    },
+  }
 }];
+// var data1 = [{
+//   users: ['Jay'],
+//   actors: ['Testy McTest', 'Rob Schnider'],
+//   directors: ['Stanley Kubrick', 'Dr. Testenstein']
+// }];
+
+
 app.post("/", function(request, response) {
-  response.json({data});
+  let user = request.body.data.user;
+  let actor = request.body.data.actor;
+  let movie = request.body.data.movie;
+  let director = request.body.data.director;
+  if (!data[0].users[user].actors.includes(actor) && actor !== '') {
+    data[0].users[user].actors.push(actor)
+  }
+  if (!data[0].users[user].movies.includes(movie) && movie !== '') {
+    data[0].users[user].movies.push(movie)
+  }
+  if (!data[0].users[user].directors.includes(director) && director !== '') {
+    data[0].users[user].directors.push(director)
+  }
 })
+app.post("/users/", function(request, response) {
+  console.log(request.body);
+    data[0].users[request.body.data.newUser] = {
+      actors: [],
+      directors: [],
+      movies: []
+    };
+    if (!data[0].profiles.includes(request.body.data.newUser)) {
+      data[0].profiles.push(request.body.data.newUser);
+    }
+})
+app.post("/current/", function(request, response){
+  console.log(request.body.data.selectedUser);
+  data[0].currentUser = request.body.data.selectedUser;
+});
+
+app.post("/favorites/", function(request, response){
+  let favorite = request.body.data.favorite;
+  if (request.body.data.actor == 'on') {
+    data[0].users[data[0].currentUser].actors.push(favorite);
+  }
+  if (request.body.data.director == 'on') {
+    data[0].users[data[0].currentUser].directors.push(favorite);
+  }
+  if (request.body.data.movie == 'on') {
+    data[0].users[data[0].currentUser].movies.push(favorite);
+  }
+});
+
+
+app.get("/current/", function(request, response){
+  console.log('hi');
+});
+
 app.get("/", function(request, response) {
-  response.json({data});
+  response.json(data)
+})
+app.get("/favorites/", function(request, response){
+  response.json(data)
+})
+
+app.get("/users/", function(request, response) {
+  let a = response.json(data);
+  console.log(a.users);
 })
 
 app.use(notFound)
